002a179955c70deef570a0a4d092bec652878346
vvoody <vvoodywang@gmail.com>
2012-09-26 14:44:28 +0200

    CHECKLIST.sec: added one new security guide link core-functions.sh: use 'rss' instead of 'size' in the ps command to                    get real memory usage of per process.

--- skel/bin/core-functions.sh
+++ skel/bin/core-functions.sh
@@ -80,5 +80,5 @@ function list_installed_pkgs() {
 # memory usage per process & sorted(desc)
 # 'sort' is not necessary, ps --sort size(no '-') works.
 function mu() {
-    ps -eo size,pid,user,command --sort -size | awk '{ hr=$1/1024 ; printf("%13.2f Mb ",hr) } { for ( x=4 ; x<=NF ; x++ ) { printf("%s ",$x) } print "" }' | sort -n -k 1
+    ps -eo rss,pid,user,command --sort -rss | awk '{ hr=$1/1024 ; printf("%13.2f Mb ",hr) } { for ( x=4 ; x<=NF ; x++ ) { printf("%s ",$x) } print "" }' | sort -n -k 1
 }
